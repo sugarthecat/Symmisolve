8c1ea2502dd0c0b3296359d9a8675c4eeec76d9f
Tj Nickerson <tjnickerson@meta.com>
2025-06-23 16:13:14 -0700

    Add React Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <Router>
       <div className="app">
         <div className='upper-container'>
           <header>
@@ -12,12 +12,18 @@ function App() {
             <p>Meta University - Engineering</p>
             <p>A Community Based SAT Solver</p>
           </header>
+          <Switch>
+            <Router exact path="/"> <p>Home</p></Router>
+            <Router path="/problem/:problemId"> <p>Problem</p></Router>
+            <Router path="/user/:userId"> <p>User</p></Router>
+            <Router path="/problem/upload"> <p>Home</p></Router>
+          </Switch>
         </div>
         <footer>
           <p>By Timothy James Nickerson, through Meta University</p>
         </footer>
       </div>
-    </>
+    </Router>
   )
 }
 
