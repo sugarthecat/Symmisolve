e7f78f2b6fa7c51a226d6c483e238e95a81416e1
Tj Nickerson <tjnickerson@meta.com>
2025-07-11 11:08:46 -0700

    Added an activity status to the database

--- server/prisma/migrations/20250630214320_symmisolve_2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `poster` on the `Problem` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Problem" DROP COLUMN "poster";
