45c7e77c6fbd12058f0d3888922b675110542795
Tj Nickerson <tjnickerson@meta.com>
2025-07-16 16:01:24 -0700

    Added problem card gradients

--- client/src/components/ProblemCard.jsx
+++ client/src/components/ProblemCard.jsx
@@ -1,21 +1,25 @@
-import { Link, } from 'react-router-dom'
-import "./ProblemCard.css"
+import { Link } from "react-router-dom";
+import "./ProblemCard.css";
 
 function ProblemCard({ problem }) {
     return (
-        <div className='problem-card'>
-            <h3><Link to={`problem/${problem.id}`}>{problem.name} {!problem.is_active && `[SOLVED]`}</Link></h3>
-            <p>
-                <b>Created By {problem.user.username}</b>
-            </p>
-            <p>
-                Size: {problem.current_size}
-            </p>
-            <p>
-                {problem.description.length > 100 ? problem.description.substring(0, 100) + "..." : problem.description}
-            </p>
-        </div>
-    )
+        <Link className="no-decor" to={`problem/${problem.id}`}>
+            <div className="problem-card">
+                <h3>
+                    {problem.name} {!problem.is_active && `[SOLVED]`}
+                </h3>
+                <p>
+                    <b>Created By {problem.user.username}</b>
+                </p>
+                <p>Size: {problem.current_size}</p>
+                <p>
+                    {problem.description.length > 100
+                        ? problem.description.substring(0, 100) + "..."
+                        : problem.description}
+                </p>
+            </div>
+        </Link>
+    );
 }
 
-export default ProblemCard
+export default ProblemCard;
