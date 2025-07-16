08d741c890bc6dc72ae42e3a0bd7bc77331fb1be
Tj Nickerson <tjnickerson@meta.com>
2025-07-16 16:27:49 -0700

    Added nit feedback

--- client/src/components/VariableAssignment.jsx
+++ client/src/components/VariableAssignment.jsx
@@ -2,9 +2,7 @@ const VariableAssignmentComponent = ({ assignment, clickFunc }) => {
     return (
         <span
             className={assignment < 0 ? "assignment assignment-overline" : "assignment"}
-            onClick={() => {
-                clickFunc();
-            }}
+            onClick={clickFunc}
         >
             {Math.abs(assignment)}
         </span>
