c59b8875d1685dea24aa318365fe9d82e44d44e6
Tj Nickerson <tjnickerson@meta.com>
2025-07-18 16:40:34 -0700

    Added display protocol for user editor

--- client/src/pages/AdminPanel.jsx
+++ client/src/pages/AdminPanel.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 
 function AdminPanelPage({ updateUser }) {
-
+    const [user, setUser] = useState(null);
+    const accessLevels = ["Regular User", "Researcher", "Admin"];
     return (
         <div>
             <h1>Admin Panel</h1>
+            <h2>User Access Level Editor</h2>
+            {user !== null && (
+                <>
+                    {user.username},{user.access_level}
+                </>
+            )}
         </div>
-    )
+    );
 }
 
-export default AdminPanelPage
+export default AdminPanelPage;
