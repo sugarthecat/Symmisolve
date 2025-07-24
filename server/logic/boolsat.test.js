5e2a72b40f39464859a2e0d8e33003f76a8e25ec
Tj Nickerson <tjnickerson@meta.com>
2025-07-23 17:55:43 -0700

    Added extra test cases

--- server/logic/boolsat.test.js
+++ server/logic/boolsat.test.js
@@ -7,12 +7,15 @@ import assert from 'node:assert/strict';
 import { getSizeCNF, optimizeCNF, sortClauses, stringifyCNF, validateSymmetry } from './boolsat.js';
 import testVerifyAssignment from './testSuites/testVerifyAssignment.test.js';
 import testVerifyConflict from './testSuites/testVerifyConflict.test.js';
+import testCNFOptimization from './testSuites/testCNFOptimization.test.js';
 
 describe('CNF Validation', testCNFValidation);
 
 describe('CNF Parsing', testCNFParsing);
 
 describe('CNF Reduction', testCNFReduction);
+
+describe('CNF Optimization', testCNFOptimization);
 
 describe('CNF Symmetry', testVerifySymmetry);
 
@@ -77,78 +80,3 @@ describe('CNF Size', () => {
         )
     })
 });
-
-describe('Formula Optimization', () => {
-    //TODO add symmetry breaking procedures
-    it.skip('Solves the pidgeonhole principle', () => {
-        //pidgeonhole principle of 3 pidgeons in 2 holes
-        assert.deepEqual(
-            optimizeCNF([
-                [1, 2],
-                [3, 4],
-                [5, 6],
-                [-1, -3],
-                [-1, -5],
-                [-3, -5],
-                [-2, -4],
-                [-2, -6],
-                [-4, -6]]
-            ),
-            [[]]
-        )
-        //pidgeonhole principle of 4 pidgeons in 3 holes
-        assert.deepEqual(
-            optimizeCNF([
-                [1, 2, 3],
-                [4, 5, 6],
-                [7, 8, 9],
-                [10, 11, 12],
-                [-1, -4],
-                [-1, -7],
-                [-1, -10],
-                [-4, -7],
-                [-4, -10],
-                [-7, -10],
-                [-2, -5],
-                [-2, -8],
-                [-2, -11],
-                [-5, -8],
-                [-5, -11],
-                [-8, -11],
-                [-3, -6],
-                [-3, -9],
-                [-3, -12],
-                [-6, -9],
-                [-6, -12],
-                [-9, -12]]
-            ),
-            [[]]
-        )
-    })
-    it.skip('Solves Transitivity problems', () => {
-        //a = b, b = c, a != c has a contradiction
-        assert.deepEqual(
-            optimizeCNF([
-                [1, -2],
-                [-1,2],
-                [2, -3],
-                [-2,3],
-                [1, 3],
-                [-1,-3],
-            ]),
-            [[]]
-        )
-        //a != b, b != c, a != c has a contradiction
-        assert.deepEqual(
-            optimizeCNF([
-                [1, 2],
-                [-1,-2],
-                [2, 3],
-                [-2,-3],
-                [1, 3],
-                [-1,-3],
-            ]),
-            [[]]
-        )
-    })
-})
